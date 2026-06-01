15021a4df826bea3852e9a8cbe808c129facd4a3
Di889 <88069861+Di889@users.noreply.github.com>
2026-06-01 13:59:26 -0300

    feat: add Unit and StatusEffect base classes

---
+++ core/src/main/java/com/diogenes/wolfpack/effects/StatusEffect.java
@@ -0,0 +1,28 @@
+﻿package com.diogenes.wolfpack.effects;
+
+import com.diogenes.wolfpack.entities.Unit;
+
+abstract public class StatusEffect {
+    protected String name;
+    protected int duration;
+
+    public StatusEffect(String name, int duration){
+        this.name = name;
+        this.duration = duration;
+    }
+
+    public boolean isExpired(){
+        return duration <= 0;
+    }
+
+    public void tick(){
+        duration--;
+        if(duration < 0) {
+            duration = 0;
+        }
+    }
+
+    abstract public void applyEffect(Unit target);
+
+    abstract public void onRemove(Unit target);
+}
---
+++ core/src/main/java/com/diogenes/wolfpack/entities/Unit.java
@@ -0,0 +1,60 @@
+﻿package com.diogenes.wolfpack.entities;
+
+import com.diogenes.wolfpack.effects.StatusEffect;
+
+import java.util.List;
+
+public abstract class Unit {
+
+    protected String name;
+    protected int level;
+    protected int xp; // TODO: sera refatorado pra apenas os playables charactears
+
+    protected int maxHp;
+    private int hp;
+    protected int attack;
+    protected int defense;
+    protected int speed;
+
+    protected List<StatusEffect> statusEffects; // TODO: inicializar como arraylist no construtor
+
+    public Unit(String name, int maxHp, int attack, int defense, int speed){
+        this.name = name;
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+        this.attack = attack;
+        this.defense = defense;
+        this.speed = speed;
+        this.xp = 0;
+        this.level = 1;
+    }
+
+    public boolean isAlive(){
+        return hp > 0;
+    }
+
+    public int takeDamage(int damage){
+        int realDamage = damage - defense;
+        if(realDamage < 0) {
+            realDamage = 0;
+        }
+        int newHp = hp - realDamage;
+        if(newHp < 0){
+            newHp = 0;
+        }
+        hp = newHp;
+        return realDamage;
+    }
+
+    public int heal(int healAmount){
+        int newHp = healAmount + hp;
+        if(newHp > maxHp){
+            newHp = maxHp;
+        }
+        healAmount = newHp - hp;
+        hp = newHp;
+        return healAmount;
+    }
+
+
+}
